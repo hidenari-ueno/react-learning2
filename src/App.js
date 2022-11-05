8d63af84653dd58f18bc6f21a8685000655dc5e2
hide Lewis ueno <44520933+hidenari-ueno@users.noreply.github.com>
2022-11-05 17:28:21 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Users } from "./components/pages/Users";
+import { Router } from "./router/Router";
+import { UserProvider } from "./providers/UserProvider";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  );
+}
